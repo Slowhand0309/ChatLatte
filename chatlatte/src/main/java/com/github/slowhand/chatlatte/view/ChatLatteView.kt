67889cd2e5b457724876ca7b5ddbbce4d4a72ea3
Slowhand0309 <slowhand0309@gmail.com>
2019-06-25 01:14:40 +0900

    Add sendMessage method

--- chatlatte/src/main/java/com/github/slowhand/chatlatte/view/ChatLatteView.kt
+++ chatlatte/src/main/java/com/github/slowhand/chatlatte/view/ChatLatteView.kt
@@ -3,6 +3,7 @@ package com.github.slowhand.chatlatte.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.OneShotPreDrawListener.add
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +16,8 @@ import com.github.slowhand.chatlatte.models.UserId
 import kotlinx.android.synthetic.main.chat_latte_view.view.*
 
 class ChatLatteView: ConstraintLayout {
+
+    /* -- public methods and properties -- */
 
     // all messages
     private var _messages: MutableList<Message> = ArrayList()
@@ -32,6 +35,11 @@ class ChatLatteView: ConstraintLayout {
         }
         get() = adapter.self
 
+    // scroll to end position via send text
+    var autoScrollEnabled = true
+
+    // clear text field via send text
+    var autoClearTextEnabled = true
 
     // members
     fun join(user: User) = adapter.apply {
@@ -43,6 +51,23 @@ class ChatLatteView: ConstraintLayout {
             members.clear()
             notifyDataSetChanged()
         }
+
+    fun sendMessage(message: Message) {
+        _messages.add(message)
+        adapter.notifyDataSetChanged()
+
+        if (autoScrollEnabled) messageListView.scrollToEnd()
+        if (autoClearTextEnabled) inputEditText.text.clear()
+    }
+
+    fun setOnClickSendButtonListener(listener: (view: View) -> Unit) {
+        sendButton.setOnClickListener { listener(it) }
+    }
+
+    fun setPlaceholder(message: String) {
+        inputEditText.hint = message
+    }
+
 
     private val adapter: MessageListAdapter by lazy {
         MessageListAdapter(context, _messages)
@@ -60,5 +85,9 @@ class ChatLatteView: ConstraintLayout {
         messageListView.adapter = adapter
         messageListView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         adapter.notifyDataSetChanged()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
     }
 }
